e2697e87ff4cdc5437df34565a536fbbeb139d82
Dustin <dustinsinkey1@gmail.com>
2016-01-10 20:42:06 -0800

    Added test.sh file

---
+++ scripts/test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+BASE_DIR='dirname $0'
+echo ""
+echo "Starting Karma Server (http://karma-runner.github.io"
+echo "----------------------------------------------------"
+
+karma start $BASE_DIR/../conf/karma.conf.js $*
