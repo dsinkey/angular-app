6d03ce0a9d630ec8f160564652564e9f003fffc4
Dustin <dustinsinkey1@gmail.com>
2016-01-11 21:39:56 -0800

    Edited conf and test.sh files

--- test/unit/controllers.spec.js
+++ test/unit/controllers.spec.js
@@ -1,16 +1,16 @@
 'use strict';
 
-describe('controller specs', function(){
-	var $scope;
+describe('controller specs', function() {
+  var $scope;
 
-	beforeEach(module('myApp.controllers'));
-
-	beforeEach(inject(function($rootScope, $controller){
-		$scope = $rootScope.$new();
-		$controller('helloWorldCtrl', {$scope: $scope});
-	}));
-
-	it('should create "name" model with first name "Dustin"', function(){
-		expect($scope.name.first).toBe("Dustin");
-	});
+  beforeEach(module('myApp.controllers'));
+ 
+  beforeEach(inject(function($rootScope, $controller) {
+    $scope = $rootScope.$new();
+    $controller('helloWorldCtrl', {$scope: $scope});
+  }));
+ 
+  it('should create "name" model with first name "Jane"', function() {
+    expect($scope.name.first).toBe("Jane");
+  });
 });
