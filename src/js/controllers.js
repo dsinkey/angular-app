188003b0767f07eff059c0501199559744b74930
Dustin <dustinsinkey1@gmail.com>
2016-01-12 20:48:30 -0800

    Changed index.html to have input for first, last name, and phone number. Small changes to margin in main.css

--- src/js/controllers.js
+++ src/js/controllers.js
@@ -4,12 +4,13 @@
 angular.module('myApp.controllers', [])
  .controller('helloWorldCtrl',function ($scope) {
         $scope.person = {
-        	first: "Dustin",
-        	last: "Sinkey"
+        	firstName: "Dustin",
+        	lastName: "Sinkey"
         };
 
         $scope.mask = "(999) 999-9999 ext 99";
-        $scope.getModel = function(){
+
+        $scope.getModel = function (){
         	return JSON.stringify($scope.person, undefined, 2);
         };
  });
