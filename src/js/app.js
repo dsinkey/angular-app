4bb7b9b4a4db22edde4e65a82c3f02fe8491c746
Dustin <dustinsinkey1@gmail.com>
2016-01-13 21:38:51 -0800

    Configured app.js to use tooltip

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,8 @@
 'use strict';
 
-angular.module('myApp', ['myApp.controllers', 'myApp.directives', 'ui.utils']);
+angular.module('myApp', ['myApp.controllers', 'myApp.directives','ui.utils'])
+  .value('uiJqConfig',{
+  	tooltip: {
+  		placement: 'bottom'
+  	}
+});
