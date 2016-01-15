f9389ee515532bec8021899948f2cff4ab91ae4c
Dustin <dustinsinkey1@gmail.com>
2016-01-14 17:36:50 -0800

    Changed some stuff in CSS, Grunt, Index.html

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,18 +4,22 @@ module.exports = function(grunt) {
     concat: {
       options: { separator: ';' },
       dist: {
-        src: [ 'src/bower_components/jquery/jquery.js',
-               'src/bower_components/angular/angular.js',
-               'src/bower_components/angular-ui-utils/ui-utils.js',
-               'src/bower_components/angular-ui-map/ui-map.js',
+        src: [ 'src/bower/jquery/jquery.js',
+               'src/bower/jquery-ui/ui/jquery-ui.js',
+               'src/bower/fullcalendar/calendar.js',
+               'src/bower/momentsjs/momemnt.js',
+               'src/bower/angular/angular.js',
+               'src/bower/angular-ui-calendar/src/calendar.js',
                'src/js/app.js', 
-               'src/js/controllers.js'],
+               'src/js/controllers.js',
+               'src/js.filters.js'],
         dest: 'dist/js/<%= pkg.name %>.js'
       }
     },
     copy: {
       main: {
-          files: [ { expand: true, cwd: 'src/css/', src: ['**'], dest: 'dist/css/' } ]
+          files: [ { expand: true, cwd: 'src/css/', src: ['**'], dest: 'dist/css/' },
+                   { expand: true, cwd: 'src/bower_components/fullcalendar', src: ['*.css'], dest: 'dist/css/' }]
       }
     },
     targethtml: {
